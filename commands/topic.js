7517b92454c09a0c5053b496881f75d8b7ceea59
Stephen A. Wilson <Stephen-356@hotmail.com>
2017-11-09 21:36:21 -0500

    add close and reopen commands

--- commands/topic.js
+++ commands/topic.js
@@ -1,5 +1,7 @@
 const Rx = require('rx');
 
+const util = require('./../lib/utilities');
+
 const OPEN_TOPICS_CAT = '!topic';
 
 module.exports = {
@@ -18,18 +20,18 @@ module.exports = {
   run(context, response) {
     let channelName = context.args.channelName;
 
-    let openCategory = getCategory(context.guild, OPEN_TOPICS_CAT);
+    let openCategory = util.findCategory(context.guild, OPEN_TOPICS_CAT);
     if (!openCategory) {
       response.type = 'message';
       response.content =
-        "My apologies, but I was not able to find the Open Discussions category.\n" +
+        "My apologies, but I was not able to find the open topics category.\n" +
         "Please let SpyMaster know about the issue";
       return response.send();
     }
 
     return Rx.Observable
       .fromPromise(context.guild.createChannel(channelName, 'text', { parent: openCategory }))
-      .flatMap((channel) => channel.setPosition(0))
+      .flatMap((channel) => channel.setPosition(1))
       .flatMap((channel) => {
         response.type = 'reply';
         response.content = 'I have opened the channel ' + channel.toString() + '.';
@@ -40,26 +42,22 @@ module.exports = {
 
         if (error.name === 'DiscordAPIError') {
           if (error.message === "Missing Permissions") {
-            response.content =
-              'I\'m sorry, but I do not have permission to create channels. I need the "Manage Channels" permission.';
+            response.content = `I'm sorry, but I do not have permission to create channels. I need the "Manage Channels" permission.`;
+          }
+          else if (error.message.includes("Invalid Form Body")) {
+            response.content = `I'm sorry, Discord does not allow that channel name.`;
           }
           else {
-            response.content =
-              'I\'m sorry, but Discord returned an unexpected error when I tried to create the channel.';
+            response.content = `I'm sorry, Discord returned an unexpected error when I tried to create the channel.`;
+            context.nix.handleError(context, error, false);
           }
         }
         else {
-          response.content =
-            'I\'m sorry, but an unexpected problem occurred.';
+          response.content = `I'm sorry, I ran into an unexpected problem.`;
+          context.nix.handleError(context, error, false);
         }
 
         return response.send();
       });
   },
 };
-
-function getCategory(guild, name) {
-  return guild.channels
-    .filter((c) => c.type === 'category')
-    .find((c) => c.name.toLowerCase().includes(name.toLowerCase()));
-}
