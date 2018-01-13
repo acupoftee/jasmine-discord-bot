e09519c6912362c588ade435f420349da90cb0b2
Stephen A. Wilson <Stephen-356@hotmail.com>
2018-01-13 18:20:08 -0500

    add netModLog token

--- modules/ow-mains/utility.js
+++ modules/ow-mains/utility.js
@@ -4,6 +4,7 @@ const DATAKEYS = {
   BROADCAST_BLIZZARD: 'owMains.broadcast.blizzard',
   BROADCAST_NETWORK: 'owMains.broadcast.network',
   BROADCAST_TOKENS: 'owMains.broadcast.tokens',
+  NET_MOD_LOG_TOKEN: 'owMains.netModLog.token',
 };
 
 const BROADCAST_TYPES = {
