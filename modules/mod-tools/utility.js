cd925d2de9d3f77f4a029c5263ba12d275e27cc5
Stephen A. Wilson <Stephen-356@hotmail.com>
2018-01-19 20:12:53 -0500

    create list of log types

--- modules/mod-tools/utility.js
+++ modules/mod-tools/utility.js
@@ -6,9 +6,22 @@ const DATAKEYS = {
 const ERRORS = {
   USER_NOT_FOUND: "User could not be found.",
   USER_ALREADY_BANNED: "User is already banned.",
+  INVALID_LOG_TYPE: "Invalid log type.",
 };
+
+const LOG_TYPES = [
+  {
+    name: 'ModLog',
+    channelDatakey: DATAKEYS.MOD_LOG_CHANNEL,
+  },
+  {
+    name: 'JoinLog',
+    channelDatakey: DATAKEYS.JOIN_LOG_CHANNEL,
+  },
+];
 
 module.exports = {
   DATAKEYS,
   ERRORS,
+  LOG_TYPES,
 };
