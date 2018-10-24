42c6604f9de04ab4ca9a6c1888fc5865eab8aadf
Stephen A. Wilson <Stephen-356@hotmail.com>
2018-10-23 20:52:08 -0400

    add an esports broadcast type

--- modules/ow-mains/utility.js
+++ modules/ow-mains/utility.js
@@ -1,16 +1,16 @@
 const config = require('../../config/config.js');
 
 const DATAKEYS = {
-  BROADCAST_BLIZZARD: 'owMains.broadcast.blizzard',
-  BROADCAST_NETWORK: 'owMains.broadcast.network',
+  BROADCAST: (type) => `owMains.broadcast.${type}`,
   BROADCAST_TOKENS: 'owMains.broadcast.tokens',
   NET_MOD_LOG: 'owMains.netModLog.channel',
   NET_MOD_LOG_TOKEN: 'owMains.netModLog.token',
 };
 
 const BROADCAST_TYPES = {
-  'blizzard': DATAKEYS.BROADCAST_BLIZZARD,
-  'network': DATAKEYS.BROADCAST_NETWORK,
+  'blizzard': DATAKEYS.BROADCAST('blizzard'),
+  'network': DATAKEYS.BROADCAST('network'),
+  'esports': DATAKEYS.BROADCAST('esports'),
 };
 
 const BROADCAST_TOKENS = config.broadcastTokens;
