4bda9c225d239c52a9974880f446b81cc25029a3
Stephen A. Wilson <Stephen-356@hotmail.com>
2018-12-04 20:06:21 -0500

    autoload all module components

--- modules/streaming/index.js
+++ modules/streaming/index.js
@@ -1,21 +1,27 @@
+const glob = require('glob');
+
 const DATAKEYS = require('./datakeys');
+
+const NOT_TEST_FILES_REGEX = /^(?!.*test\.js).*\.js$/;
 
 module.exports = {
   name: 'streaming',
-  permissions: [
-    'broadcaster',
-  ],
   defaultData: [
     {keyword: DATAKEYS.WATCH_ROLE, data: null},
     {keyword: DATAKEYS.RESTRICT_GAMES, data: false},
     {keyword: DATAKEYS.ALLOWED_GAMES, data: null},
     {keyword: DATAKEYS.LIVE_ROLE, data: null},
   ],
-  services: [
-    require('./services/streaming-service'),
-  ],
-  configActions: [
-  ],
-  commands: [
-  ],
+  services: glob
+    .sync(`${__dirname}/services/**/*.js`)
+    .filter((filename) => NOT_TEST_FILES_REGEX.test(filename))
+    .map((filename) => require(filename)),
+  configActions: glob
+    .sync(`${__dirname}/config/**/*.js`)
+    .filter((filename) => NOT_TEST_FILES_REGEX.test(filename))
+    .map((filename) => require(filename)),
+  commands: glob
+    .sync(`${__dirname}/commands/**/*.js`)
+    .filter((filename) => NOT_TEST_FILES_REGEX.test(filename))
+    .map((filename) => require(filename)),
 };
