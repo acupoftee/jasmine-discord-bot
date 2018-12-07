5c2c9ae7817f1bcebb193beecb4b379a84d20023
Stephen A. Wilson <Stephen-356@hotmail.com>
2018-12-07 16:39:48 -0500

    move findRole to common library file

--- modules/streaming/config/set-live-role.js
+++ modules/streaming/config/set-live-role.js
@@ -1,4 +1,6 @@
-let Rx = require('rx');
+const Rx = require('rx');
+
+const { findRole } = require("../../../lib/role-utilities");
 
 module.exports = {
   name: 'setLiveRole',
@@ -42,22 +44,3 @@ module.exports = {
       }));
   },
 };
-
-function findRole(guild, roleString) {
-  let idRegex = /^\d+$/;
-  let mentionRegex = /^<@&?(\d+)>$/;
-
-  if (roleString.match(idRegex)) {
-    // string is an role ID
-    return guild.roles.get(roleString);
-  }
-
-  let matches = roleString.match(mentionRegex);
-  if (matches) {
-    // string is an role mention
-    return guild.roles.get(matches[1]);
-  }
-
-  // string is a role name
-  return guild.roles.find((r) => r.name.toLowerCase() === roleString.toLowerCase());
-}
